942d0df24ac20c1e55a1b0765e7f6d5bc4d3d2fb
1282230766 <1282230766@qq.com>
2019-02-21 13:04:48 +0800

    增加firstController

---
+++ src/main/java/com/springboot5/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.springboot5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FirstController {
+
+    @RequestMapping("/first")
+    public ModelAndView first() {
+        ModelAndView mv = new ModelAndView();
+        return mv;
+    }
+}
